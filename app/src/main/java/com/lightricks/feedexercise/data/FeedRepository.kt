1842b62c0c960eef43e448c4f5fd4d043ba70a61
Yitz <dlefkowitz@lightricks.com>
2022-06-30 16:49:32 +0300

    Create FeedRepo and add to DI graph

--- app/src/main/java/com/lightricks/feedexercise/data/FeedRepository.kt
+++ app/src/main/java/com/lightricks/feedexercise/data/FeedRepository.kt
@@ -1,9 +1,40 @@
 package com.lightricks.feedexercise.data
 
+import com.lightricks.feedexercise.network.FeedApiService
+import kotlinx.coroutines.flow.Flow
+import kotlinx.coroutines.flow.emptyFlow
+import javax.inject.Inject
+import javax.inject.Singleton
+
 /**
  * This is our data layer abstraction. Users of this class don't need to know
  * where the data actually comes from (network, database or somewhere else).
  */
-class FeedRepository {
-    //todo: implement
+interface FeedRepository {
+    fun getFeed(): Flow<List<FeedItem>>
+    suspend fun refreshFeed(): RefreshFeedResult
+}
+
+sealed class RefreshFeedResult {
+    object Success : RefreshFeedResult()
+    data class Error(val error: Throwable) : RefreshFeedResult()
+}
+
+@Singleton
+class FeedRepositoryImpl @Inject constructor(
+    private val feedApi: FeedApiService
+): FeedRepository {
+
+    override fun getFeed(): Flow<List<FeedItem>> = emptyFlow()
+
+    override suspend fun refreshFeed(): RefreshFeedResult {
+        val response = feedApi.getFeed()
+        return try {
+            // map the feed items
+            // cache the feed items
+            RefreshFeedResult.Success
+        } catch (e: Exception) {
+            RefreshFeedResult.Error(e)
+        }
+    }
 }
--- app/src/main/java/com/lightricks/feedexercise/di/DataModule.kt
+++ app/src/main/java/com/lightricks/feedexercise/di/DataModule.kt
@@ -1,8 +1,11 @@
 package com.lightricks.feedexercise.di
 
 import com.jakewharton.retrofit2.converter.kotlinx.serialization.asConverterFactory
+import com.lightricks.feedexercise.data.FeedRepository
+import com.lightricks.feedexercise.data.FeedRepositoryImpl
 import com.lightricks.feedexercise.data.OkHttpClientFactory
 import com.lightricks.feedexercise.network.FeedApiService
+import dagger.Binds
 import dagger.Module
 import dagger.Provides
 import dagger.hilt.InstallIn
@@ -38,4 +41,12 @@ object DataModule {
     @Provides
     fun provideFeedApiService(retrofit: Retrofit): FeedApiService =
         retrofit.create(FeedApiService::class.java)
+}
+
+@Module
+@InstallIn(SingletonComponent::class)
+abstract class DataBindModule {
+    @Singleton
+    @Binds
+    abstract fun provideFeedRepository(feedRepository: FeedRepositoryImpl): FeedRepository
 }
