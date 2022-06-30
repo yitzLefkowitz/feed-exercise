e752ea781e936ba998e66b8699043ef059da6621
Yitz <dlefkowitz@lightricks.com>
2022-06-30 16:38:48 +0300

    Create feed api

--- app/src/main/java/com/lightricks/feedexercise/network/DataTransferObjects.kt
+++ app/src/main/java/com/lightricks/feedexercise/network/DataTransferObjects.kt
@@ -1,5 +1,12 @@
 package com.lightricks.feedexercise.network
 
-/**
- * todo: add Data Transfer Object data class(es) here
- */
+import kotlinx.serialization.SerialName
+import kotlinx.serialization.Serializable
+
+@Serializable
+data class FeedItemDto(
+    val id: String,
+    @SerialName("templateThumbnailURI")
+    val thumbnailUrl: String,
+    val isPremium: Boolean
+)
