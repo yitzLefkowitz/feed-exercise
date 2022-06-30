cb755d012648cfebc53a25c37dd5c600e367f3b9
Yitz <dlefkowitz@lightricks.com>
2022-06-30 16:10:55 +0300

    init timber

--- app/src/main/java/com/lightricks/feedexercise/FeedApplication.kt
+++ app/src/main/java/com/lightricks/feedexercise/FeedApplication.kt
@@ -2,6 +2,15 @@ package com.lightricks.feedexercise
 
 import android.app.Application
 import dagger.hilt.android.HiltAndroidApp
+import timber.log.Timber
 
 @HiltAndroidApp
-class FeedApplication : Application()
+class FeedApplication : Application() {
+
+    override fun onCreate() {
+        super.onCreate()
+        if (BuildConfig.DEBUG) {
+            Timber.plant(Timber.DebugTree())
+        }
+    }
+}
