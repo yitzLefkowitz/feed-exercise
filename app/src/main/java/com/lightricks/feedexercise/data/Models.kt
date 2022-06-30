a3236ce0768ed8ca806538f0ef877dcbd21efb80
Yitz <dlefkowitz@lightricks.com>
2022-06-30 16:38:48 +0300

    Create feed api

--- app/src/main/java/com/lightricks/feedexercise/data/Models.kt
+++ app/src/main/java/com/lightricks/feedexercise/data/Models.kt
@@ -1,13 +1,11 @@
 package com.lightricks.feedexercise.data
 
-import kotlinx.serialization.Serializable
 
 /**
  * This data class is our internal representation of a feed item.
  * In a real-life application this is template meta-data for a user's project.
  * The rest of the properties are left out for brevity.
  */
-@Serializable
 data class FeedItem(
     val id: Int = 0,
     val feedItemId: String,
